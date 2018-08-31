aff13c10619b1b3055e28be15dabb60fe74ca9f1
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-08-31 16:08:30 -0500

    WIP first cut on docker-compose

--- config/config.docker.js
+++ config/config.docker.js
@@ -5,7 +5,19 @@ const validateConfig = require('webgme/config/validator');
 // This is only for testing - it will persist everything inside the containers.
 
 config.plugin.allowServerExecution = true;
-config.mongo.uri = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':' + process.env.MONGO_PORT_27017_TCP_PORT + '/webgme-icore';
+config.mongo.uri = 'mongodb://' + process.env.MONGO_IP + ':27017/webgme_icore';
+config.server.workerManager.path = 'webgme-docker-worker-manager';
+
+config.server.workerManager.options = {
+    webgmeUrl: 'http://' + process.env.WEBGME_IP + ':' + config.server.port,
+    image: 'icore_py-core-executor',
+    maxRunningContainers: 10,
+    createParams: {
+        HostConfig: {
+            Memory: 536870912
+        }
+    }
+};
 
 
 validateConfig(config);
