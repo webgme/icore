2e5659e5d71f273fcf0e60a54e460dfb51821c4a
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-09-07 13:25:07 -0500

    WIP isolate database from workers

--- config/config.docker.js
+++ config/config.docker.js
@@ -5,11 +5,11 @@ const validateConfig = require('webgme/config/validator');
 // This is only for testing - it will persist everything inside the containers.
 
 config.plugin.allowServerExecution = true;
-config.mongo.uri = 'mongodb://' + process.env.MONGO_IP + ':27017/webgme_icore';
+config.mongo.uri = 'mongodb://mongo:27017/webgme_icore';
 config.server.workerManager.path = 'webgme-docker-worker-manager';
 
 config.server.workerManager.options = {
-    webgmeUrl: 'http://' + process.env.WEBGME_IP + ':' + config.server.port,
+    webgmeUrl: 'http://webgme-server:' + config.server.port,
     image: 'icore_py-core-executor',
     maxRunningContainers: 10,
     createParams: {
