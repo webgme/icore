ba6294f9b7e9cd61d6b8f80da3fd47f8a6395cd9
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-09-07 14:49:53 -0500

    Network isolation python worker (#7)
    * WIP isolate database from workers
    
    * WIP define the networks as bridges, pass network at worker creation

--- config/config.docker.js
+++ config/config.docker.js
@@ -5,16 +5,17 @@ const validateConfig = require('webgme/config/validator');
 // This is only for testing - it will persist everything inside the containers.
 
 config.plugin.allowServerExecution = true;
-config.mongo.uri = 'mongodb://' + process.env.MONGO_IP + ':27017/webgme_icore';
+config.mongo.uri = 'mongodb://mongo:27017/webgme_icore';
 config.server.workerManager.path = 'webgme-docker-worker-manager';
 
 config.server.workerManager.options = {
-    webgmeUrl: 'http://' + process.env.WEBGME_IP + ':' + config.server.port,
+    webgmeUrl: 'http://webgme-server:' + config.server.port,
     image: 'icore_py-core-executor',
     maxRunningContainers: 10,
     createParams: {
         HostConfig: {
-            Memory: 536870912
+            Memory: 536870912,
+            NetworkMode: 'icore_frontend'
         }
     }
 };
